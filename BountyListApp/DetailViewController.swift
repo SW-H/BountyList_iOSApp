5dcc1c53aa91fdcbfc601e42cff8a17ec9ea9b89
SeungwonHan <hsw08@naver.com>
2021-01-26 11:12:37 +0900

    add viewModel

--- BountyListApp/DetailViewController.swift
+++ BountyListApp/DetailViewController.swift
@@ -28,18 +28,17 @@ class DetailViewController: UIViewController {
     
 //    var name:String?
 //    var bounty:Int?
-    var bountyInfo : BountyInfo?
+//    var bountyInfo : BountyInfo?
+    let viewModel = DetailViewModel()
+    
     override func viewDidLoad() { // 이 view controller 가 메모리에 올라온 시점에 호출 됨 ( 보이기 직전 )
         super.viewDidLoad()
         updateUI()
     }
     
     func updateUI(){
-        if let bountyInfo = self.bountyInfo{
-            imgView.image =  bountyInfo.image
-            nameLabel.text = bountyInfo.name
-            bountyLabel.text = "\(bountyInfo.bounty)"
-        }
+
+        
 //        if let name = self.name, let bounty = self.bounty {
 //            let img = UIImage(named: "\(name).jpg")
 //            imgView.image =  img
@@ -47,6 +46,20 @@ class DetailViewController: UIViewController {
 //            bountyLabel.text = "\(bounty)"
 //        }
 //
+
+        //        if let bountyInfo = self.bountyInfo{
+//                    imgView.image =  bountyInfo.image
+//                    nameLabel.text = bountyInfo.name
+//                    bountyLabel.text = "\(bountyInfo.bounty)"
+        //        }
+        
+        
+        if let bountyInfo = viewModel.bountyInfo{
+            imgView.image =  bountyInfo.image
+            nameLabel.text = bountyInfo.name
+            bountyLabel.text = "\(bountyInfo.bounty)"
+        }
+
     }
     
     //  버튼을 눌렀을때 이 view controller는 닫히는 액션을 넣기
@@ -54,5 +67,13 @@ class DetailViewController: UIViewController {
         dismiss(animated:true, completion:nil)
         // dismiss =  뷰컨트롤러 닫는다
         // completion = 뷰컨트롤러가 닫힌 후에 수행할 일
+    }
+}
+
+class DetailViewModel{
+    var bountyInfo : BountyInfo?
+    
+    func update(model : BountyInfo? ){
+        bountyInfo=model
     }
 }
